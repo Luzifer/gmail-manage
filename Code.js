0bcc91d1701a7b01b11eefb5f93b767a62098ecf
Knut Ahlers <knut@ahlers.me>
2018-10-20 18:05:26 +0200

    Remove social-media stuff after 2y

--- Code.js
+++ Code.js
@@ -4,6 +4,7 @@ var MAX_DELETE_PER_LOOP = 1000
 function cleanup() {
   trashByQuery("older_than:10y")
   trashByQuery("label:newsletter older_than:6m")
+  trashByQuery("label:social-media older_than:2y")
 }
 
 function trashByQuery(query) {

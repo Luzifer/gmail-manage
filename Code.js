05c5feac6a500f2273ecebf79284708d63d68890
Knut Ahlers <knut@ahlers.me>
2018-10-20 18:11:39 +0200

    Trash crontab mails after 6m

--- Code.js
+++ Code.js
@@ -2,9 +2,10 @@
 var MAX_DELETE_PER_LOOP = 1000
 
 function cleanup() {
-  trashByQuery("older_than:10y")
+  trashByQuery("label:crontab  older_than:6m")
   trashByQuery("label:newsletter older_than:6m")
   trashByQuery("label:social-media older_than:2y")
+  trashByQuery("older_than:10y")
 }
 
 function trashByQuery(query) {
